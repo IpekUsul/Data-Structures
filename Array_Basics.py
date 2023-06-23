bb857809a794076fa0bef26d6f24130b37e51263
IpekUsul <118449645+IpekUsul@users.noreply.github.com>
2023-06-23 22:16:11 +0300

    Create Array_Basics.py

---
+++ Array_Basics.py
@@ -0,0 +1,18 @@
+'''
+new_list=[1,2,3]  
+result= new_list[0]   =>shows the index
+'''
+
+#search algorithms
+
+new_list=[1,2,3]  
+result= new_list[0]
+
+if 1 in new_list: print(True)
+
+for n in new_list:
+  if n==1:
+    print True
+
+    break #allows you to exit a loop when an external condition is met. 
+
