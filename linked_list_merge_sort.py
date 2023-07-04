21b6edc0d4659b2f3765c62291cc7caadc226236
IpekUsul <118449645+IpekUsul@users.noreply.github.com>
2023-07-04 16:12:42 +0300

    Update linked_list_merge_sort.py

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -7,6 +7,7 @@ def merge_sort(linked_list):
   - Repeatedly merge the sublists to produce sorted sublists until one remains
 
   Returns a sorted linked list
+  runs in 0(kn log n)
   """
 if linked_list.size()== 1:
   return linked_list
@@ -41,5 +42,75 @@ right_half.head=mid_node.next_node
 mid_node.next_node=None
 
 return left_half, right_half
+
+def merge(left,right):
+  """
+  Merges two linked lists,sorting by data in nodes
+  returns a new,merged list
+  """
+# create a new linked list that contains nodes from 
+# merging left and right
+
+merged =LinkedList()
+
+# add a fake head that is discarded later
+merged.add(0)
+
+# set current to the head of the linked list
+current=merged.head
+
+#obtain head nodes for left and right linked lists
+left_head= left.head
+right_head= right.head
+
+# iterate over left and right until we reach the tail node
+# of either
+while left_head or right_head:
+  # if the head node of left is None,we're past the tail
+  # add the node from right to merged linked list
+  if left_head is None:
+    current.next_node= right_head
+    #call next on right to set loop condition to False
+    right_head=right_head.next_node
+  # if the head node of right is None,we're past the tail
+  # add the tail node from left to merged linked list
+  elif right_head is None:
+    current.next_node= left_head
+    # call next on left to set loop conidition to False
+    left_head=left_head.next_node
+  else:
+    # Not at either tail node 
+    # obtain node data to perform comparison operations
+    left_data= left_head.data
+    right_data= right_head.data
+    # if data on left is less than right, set current to left node
+    if left_data < right_data:
+      current.next_node=left_head
+      # move left head to next node
+      left_head= left_head.next_node
+      # if data on left is greater than right, set current to right node
+    else:
+      current.next_node=right_head
+      # move right head to next node
+      right_head=right_head.next_node
+# move current to next node
+current=current.next_node
+
+# discard fake head and set first merged node as head
+head=merged.head.next_node
+merged.head=head
+
+return merged
+
+l=LinkedList()
+l.add(10)
+l.add(2)
+l.add(44)
+l.add(15)
+l.add(200)
+
+print(l)
+sorted_linked_list=merge_sort(l)
+print(sorted_linked_list)
 
   
